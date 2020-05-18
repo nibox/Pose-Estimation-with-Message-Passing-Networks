Added batching index and correct loss calculation

src/Models/PoseEstimation/PoseEstimation.py
@@ -64,18 +64,18 @@ class PoseEstimationBaseline(nn.Module):
                                                    inclusion_radius=self.config["inclusion_radius"],
                                                    mpn_graph_type=self.config["mpn_graph_type"])
 
-        x, edge_attr, edge_index, edge_labels, joint_det, label_mask = graph_constructor.construct_graph()
+        x, edge_attr, edge_index, edge_labels, joint_det, label_mask, batch_index = graph_constructor.construct_graph()
 
         pred = self.mpn(x, edge_attr, edge_index).squeeze()
         if not with_logits:
             pred = torch.sigmoid(pred)
 
-        return pred, joint_det, edge_index, edge_labels, label_mask
+        return pred, joint_det, edge_index, edge_labels, label_mask, batch_index
 
-    def loss(self, output, targets, with_logits=True, pos_weight=None, mask=None) -> torch.Tensor:
+    def loss(self, output, targets, with_logits=True, pos_weight=None, mask=None, batch_index=None) -> torch.Tensor:
         if self.focal is not None:
             assert with_logits
-            loss = self.focal(output, targets, mask)
+            loss = self.focal(output, targets, mask, batch_index)
             return loss
 
         assert mask is None

src/Models/PoseEstimation/UpperBound.py
@@ -54,7 +54,7 @@ class UpperBoundModel(nn.Module):
                                                    inclusion_radius=self.config["inclusion_radius"],
                                                    mpn_graph_type=self.config["mpn_graph_type"])
 
-        x, edge_attr, edge_index, edge_labels, joint_det, label_mask = graph_constructor.construct_graph()
+        x, edge_attr, edge_index, edge_labels, joint_det, label_mask, batch_index = graph_constructor.construct_graph()
 
         return edge_labels, joint_det, edge_index, edge_labels, label_mask
 

src/Utils/ConstructGraph.py
@@ -29,6 +29,7 @@ class NaiveGraphConstructor:
     def construct_graph(self):
         x_list, edge_attr_list, edge_index_list, edge_labels_list, joint_det_list = [], [], [], [], []
         label_mask_list = []
+        batch_index = []
         num_node_list = [0]
         for batch in range(self.batch_size):
             if self.mask_crowds:
@@ -77,6 +78,7 @@ class NaiveGraphConstructor:
                     label_mask = label_mask - 1
                 label_mask_list.append(label_mask)
             x_list.append(x)
+            batch_index.append(torch.ones_like(edge_labels, dtype=torch.long) * batch)
             num_node_list.append(x.shape[0] + num_node_list[-1])
             edge_attr_list.append(edge_attr)
             edge_index_list.append(edge_index)
@@ -87,6 +89,7 @@ class NaiveGraphConstructor:
             edge_index_list[i] += num_node_list[i]
 
         x_list = torch.cat(x_list, 0)
+        batch_index = torch.cat(batch_index, 0)
         edge_attr_list = torch.cat(edge_attr_list, 0)
         edge_index_list = torch.cat(edge_index_list, 1)
         joint_det_list = torch.cat(joint_det_list, 0)
@@ -98,7 +101,7 @@ class NaiveGraphConstructor:
         else:
             label_mask_list = torch.cat(label_mask_list, 0)
 
-        return x_list, edge_attr_list, edge_index_list, edge_labels_list, joint_det_list, label_mask_list
+        return x_list, edge_attr_list, edge_index_list, edge_labels_list, joint_det_list, label_mask_list, batch_index
 
     def _construct_mpn_graph(self, joint_det, features, graph_type):
         """

src/Utils/Utils.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import numpy as np
 from matplotlib import pyplot as plt
 from torch_geometric.utils import dense_to_sparse
+from torch_scatter import scatter_mean
 
 from Utils.correlation_clustering.correlation_clustering_utils import cluster_graph
 from Utils.dataset_utils import Graph
@@ -87,7 +88,7 @@ class FocalLoss(nn.Module):
         self.logits = logits
         self.reduce = reduce
 
-    def forward(self, inputs, targets, mask=None):
+    def forward(self, inputs, targets, mask=None, batch_index=None):
         if self.logits:
             BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
         else:
@@ -98,6 +99,9 @@ class FocalLoss(nn.Module):
             F_loss = F_loss * mask
 
         if self.reduce:
+            if batch_index is not None:
+                F_loss = scatter_mean(F_loss, batch_index)
+                assert len(F_loss) == batch_index.max() + 1
             return torch.mean(F_loss)
         else:
             return F_loss

src/train.py
@@ -119,14 +119,14 @@ def main():
             imgs = imgs.to(device)
             masks = masks.to(device)
             keypoints = keypoints.to(device)
-            pred, joint_det, edge_index, edge_labels, label_mask = model(imgs, keypoints, masks)
+            pred, joint_det, edge_index, edge_labels, label_mask, batch_index = model(imgs, keypoints, masks)
 
             if len(edge_labels[edge_labels == 1]) != 0 and len(edge_labels[edge_labels == 0]) != 0:
                 pos_weight = torch.tensor(len(edge_labels[edge_labels == 0]) / len(edge_labels[edge_labels == 1]))
             else:
                 pos_weight = torch.tensor(1.0)
             label_mask = label_mask if use_label_mask else None
-            loss = model.loss(pred, edge_labels, pos_weight=pos_weight, mask=label_mask)
+            loss = model.loss(pred, edge_labels, pos_weight=pos_weight, mask=label_mask, batch_index=batch_index)
             loss.backward()
             optimizer.step()
             result = pred.sigmoid().squeeze()
@@ -163,7 +163,7 @@ def main():
                 imgs = imgs.to(device)
                 masks = masks.to(device)
                 keypoints = keypoints.to(device)
-                pred, joint_det, edge_index, edge_labels, label_mask = model(imgs, keypoints, masks)
+                pred, joint_det, edge_index, edge_labels, label_mask, batch_index = model(imgs, keypoints, masks)
 
                 if len(edge_labels[edge_labels == 1]) != 0:
                     pos_weight = torch.tensor(len(edge_labels[edge_labels == 0]) / len(edge_labels[edge_labels == 1]))