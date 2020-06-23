filter visible joints using mask size (instead of heatmap size) added hybrid selection approach to threshold selection -> now atleast top 5 per joint type are selected fixed masking for hrnet (HRNetMineTransformation class)
test_construct_graph
- added no grad env
adapted comp_graph_stats for new transformations

src/Utils/transforms/transforms.py
@@ -261,7 +261,7 @@ class HRNetMineTransformation(object):
                 center, scale, (new_width, new_height))[:2]
             mask[i] = cv2.warpAffine(
                 (mask[i]*255).astype(np.uint8), mat_output,
-                (_output_size, _output_size)
+                (new_width, new_height)
             ) / 255
 
             mask[i] = (mask[i] > 0.5).astype(np.float32)
@@ -270,7 +270,7 @@ class HRNetMineTransformation(object):
                 joints[i][:, :, 0:2], mat_output
             )
             matrices.append(mat_output)
-            factor = factor / 2
+            factor = factor * 2
 
         factors = self._affine_factors(factors, matrices[0])
 

src/data/CocoKeypoints_hr.py
@@ -158,7 +158,7 @@ class CocoKeypoints(Dataset):
         if self.heatmap_generator is not None:
             for scale_idx in range(self.num_scales):
                 heatmap = self.heatmap_generator[scale_idx](keypoint_list[scale_idx])
-                keypoint_list[scale_idx] = _filter_visible(keypoint_list[scale_idx], heatmap.shape[1:])
+                keypoint_list[scale_idx] = _filter_visible(keypoint_list[scale_idx], mask[scale_idx].shape)
 
                 heatmaps.append(heatmap.astype(np.float32))
                 mask_list[scale_idx] = mask_list[scale_idx].astype(np.float32)

src/graph_constructor/ConstructGraph.py
@@ -521,9 +521,23 @@ def joint_det_from_scoremap(scoremap, threshold=0.007, mask=None):
         joint_map = joint_map * mask.unsqueeze(0)
     scoremap = scoremap * joint_map
     if threshold is not None:
+        # use atleast the top 5 keypoints per type
+        k = 5
+        scoremap_shape = scoremap.shape
+        scores, indices = scoremap.view(17, -1).topk(k=k, dim=1)
+        container = torch.zeros_like(scoremap, device=scoremap.device, dtype=torch.float).reshape(17, -1)
+        container[np.arange(0, 17).reshape(17, 1), indices] = scores
+        container = container.reshape(scoremap_shape)
+        top_joint_idx_det, top_joint_y, top_joint_x = container.nonzero(as_tuple=True)
+        # top_joint_scores = container[top_joint_idx_det, top_joint_y, top_joint_x]
+
         scoremap = torch.where(scoremap < threshold, torch.zeros_like(scoremap), scoremap)
         joint_idx_det, joint_y, joint_x = scoremap.nonzero(as_tuple=True)
-        joint_scores = scoremap[joint_idx_det, joint_y, joint_x]
+
+        top_joint_pos = torch.stack([top_joint_x, top_joint_y, top_joint_idx_det], 1)
+        thresh_joint_pos = torch.stack([joint_x, joint_y, joint_idx_det], 1)
+        joint_positions_det = cat_unique(top_joint_pos, thresh_joint_pos)
+        joint_scores = scoremap[joint_positions_det[:, 2], joint_positions_det[:, 1], joint_positions_det[:, 0]]
     else:
         k = 30
         scoremap_shape = scoremap.shape
@@ -535,5 +549,18 @@ def joint_det_from_scoremap(scoremap, threshold=0.007, mask=None):
         joint_scores = container[joint_idx_det, joint_y, joint_x]
         assert len(joint_idx_det) == k * 17
 
-    joint_positions_det = torch.stack([joint_x, joint_y, joint_idx_det], 1)
+        joint_positions_det = torch.stack([joint_x, joint_y, joint_idx_det], 1)
     return joint_positions_det, joint_scores
+
+
+def cat_unique(tensor_1, tensor_2):
+    # tensor_1/2 have shape (N, D) D is dim of vector and N is number of vectors
+    assert len(tensor_1.shape) == 2 and len(tensor_2.shape) == 2
+    vector_dim = tensor_1.shape[1]
+    unique_elements = torch.eq(tensor_1.unsqueeze(1), tensor_2)
+    unique_elements = unique_elements.int().sum(dim=2)
+    unique_elements = unique_elements == vector_dim
+    unique_elements = unique_elements.sum(dim=0)
+    unique_elements = unique_elements == 0
+
+    return torch.cat([tensor_1, tensor_2[unique_elements]], 0)

src/test/comp_graph_stats.py
@@ -1,12 +1,13 @@
 import torch
 import torchvision
-from data import CocoKeypoints_hr
+from data import CocoKeypoints_hr, HeatmapGenerator
 import numpy as np
 import pickle
 from config import get_config, update_config
 from Models import get_upper_bound_model
 from Utils import num_non_detected_points, to_tensor
 from tqdm import tqdm
+from Utils.transforms import transforms_hr_eval
 
 
 
@@ -32,8 +33,11 @@ def main():
     train_ids, valid_ids = pickle.load(open("tmp/coco_17_mini_split.p", "rb"))
     ids = np.concatenate([train_ids, valid_ids])
     valid_ids = np.random.choice(valid_ids, 100, replace=False)
-    img_set = CocoKeypoints_hr(config.DATASET.ROOT, mini=True, seed=0, mode="val", img_ids=valid_ids, year=17,
-                                output_size=256)
+
+    heatmap_generator = [HeatmapGenerator(128, 17), HeatmapGenerator(256, 17)]
+    transforms, _ = transforms_hr_eval(config)
+    img_set = CocoKeypoints_hr(config.DATASET.ROOT, mini=True, seed=0, mode="train", img_ids=train_ids, year=17,
+                                transforms=transforms, heatmap_generator=heatmap_generator)
 
     num_detections = []
     num_edges = []
@@ -41,27 +45,28 @@ def main():
     imbalance = []
     deg = []
     # todo number of not detected keypoints
-    for i in tqdm(range(100)):  # just test the first 100 images
-        # split batch
-        img, masks, keypoints, factors = img_set[i]
-        img_transformed = transforms(img).to(device)[None]
-        masks, keypoints, factors = to_tensor(device, masks, keypoints, factors)
-        _, pred, joint_det, edge_index, edge_labels, _ = model(img_transformed, keypoints, masks, factors)
-        #deg.append(degree(edge_index[1], len(joint_det)).mean())
-        imbalance.append(edge_labels.mean().item())
-        num_non_detected, num_gt = num_non_detected_points(joint_det, keypoints, 6.0,
-                                                           config.MODEL.GC.USE_GT)
+    with torch.no_grad():
+        for i in tqdm(range(100)):  # just test the first 100 images
+            # split batch
+            img, _, masks, keypoints, factors = img_set[i]
+            img_transformed = img.to(device)[None]
+            masks, keypoints, factors = to_tensor(device, masks[-1], keypoints, factors)
+            _, pred, joint_det, edge_index, edge_labels, _ = model(img_transformed, keypoints, masks, factors)
+            #deg.append(degree(edge_index[1], len(joint_det)).mean())
+            imbalance.append(edge_labels.mean().item())
+            num_non_detected, num_gt = num_non_detected_points(joint_det, keypoints, 6.0,
+                                                               config.MODEL.GC.USE_GT)
 
-        num_detections.append(len(joint_det))
-        num_edges.append(len(edge_labels))
-        num_det_failures.append(float(num_non_detected) / num_gt)
-    print(f"Average number of detections:{np.mean(num_detections)}")
-    print(f"Std number of detections:{np.std(num_detections)}")
-    print(f"Average number of edges:{np.mean(num_edges)}")
-    print(f"Std number of edges:{np.std(num_edges)}")
-    print(f"Average Imbalance: {np.mean(imbalance)}")
-    print(f"Average detection failure: {np.mean(num_det_failures)}")
-    #print(f"Average node degree: {np.mean(degree)}")
+            num_detections.append(len(joint_det))
+            num_edges.append(len(edge_labels))
+            num_det_failures.append(float(num_non_detected) / num_gt)
+        print(f"Average number of detections:{np.mean(num_detections)}")
+        print(f"Std number of detections:{np.std(num_detections)}")
+        print(f"Average number of edges:{np.mean(num_edges)}")
+        print(f"Std number of edges:{np.std(num_edges)}")
+        print(f"Average Imbalance: {np.mean(imbalance)}")
+        print(f"Average detection failure: {np.mean(num_det_failures)}")
+        #print(f"Average node degree: {np.mean(degree)}")
 
 
 if __name__ == "__main__":

src/test/test_construct_graph.py
@@ -42,48 +42,49 @@ def main():
                                transforms=transforms, heatmap_generator=heatmap_generator)
 
     imgs_without_det = 0
-    for i in range(3500):  # just test the first 100 images
-        # split batch
-        img_id = img_set.img_ids[i]
-        if use_subset:
-            if img_id not in id_subset:
-                continue
-        print(f"Iter : {i}")
-        print(f"img_idx: {img_set.img_ids[i]}")
-        img, _, masks, keypoints, factor_list = img_set[i]
-        mask, keypoints, factor_list = to_tensor(device, masks[-1], keypoints, factor_list)
-        img = img.to(device)[None]
-        _, pred, joint_det, edge_index, _, label_mask = model(img, keypoints, mask, factor_list)
+    with torch.no_grad():
+        for i in range(3500):  # just test the first 100 images
+            # split batch
+            img_id = img_set.img_ids[i]
+            if use_subset:
+                if img_id not in id_subset:
+                    continue
+            print(f"Iter : {i}")
+            print(f"img_idx: {img_set.img_ids[i]}")
+            img, _, masks, keypoints, factor_list = img_set[i]
+            mask, keypoints, factor_list = to_tensor(device, masks[-1], keypoints, factor_list)
+            img = img.to(device)[None]
+            _, pred, joint_det, edge_index, _, label_mask = model(img, keypoints, mask, factor_list)
 
-        # construct poses
-        persons_pred_cc, _ = pred_to_person(joint_det, edge_index, pred, config.MODEL.GC.CC_METHOD)
-        # construct solution by using only labeled edges (instead of corr clustering)
-        sparse_sol_gt = torch.stack([edge_index[0, pred == 1], edge_index[1, pred == 1]])
-        persons_pred_gt, _ = graph_cluster_to_persons(joint_det, sparse_sol_gt)  # might crash
-        print(f"Num detection: {len(joint_det)}")
-        print(f"Num edges : {len(edge_index[0])}")
-        print(f"Num active edges: {(pred==1).sum()}")
+            # construct poses
+            persons_pred_cc, _ = pred_to_person(joint_det, edge_index, pred, config.MODEL.GC.CC_METHOD)
+            # construct solution by using only labeled edges (instead of corr clustering)
+            sparse_sol_gt = torch.stack([edge_index[0, pred == 1], edge_index[1, pred == 1]])
+            persons_pred_gt, _ = graph_cluster_to_persons(joint_det, sparse_sol_gt)  # might crash
+            print(f"Num detection: {len(joint_det)}")
+            print(f"Num edges : {len(edge_index[0])}")
+            print(f"Num active edges: {(pred==1).sum()}")
 
-        joint_det = joint_det.cpu().numpy().squeeze()
-        keypoints = keypoints.cpu().numpy().squeeze()
+            joint_det = joint_det.cpu().numpy().squeeze()
+            keypoints = keypoints.cpu().numpy().squeeze()
 
-        img = np.array(transforms_inv(img.cpu().squeeze()))
-        draw_detection(img, joint_det, keypoints,
-                       fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_det.png",
+            img = np.array(transforms_inv(img.cpu().squeeze()))
+            draw_detection(img, joint_det, keypoints,
+                           fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_det.png",
+                           output_size=256)
+            draw_poses(img, keypoints, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt.png",
                        output_size=256)
-        draw_poses(img, keypoints, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt.png",
-                   output_size=256)
-        if (pred==1).sum() == 0:
-            imgs_without_det += 1
-            continue
-        draw_clusters(img, joint_det, sparse_sol_gt, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt_full.png",
-                      output_size=256)
-        # draw_poses(imgs, persons_pred_cc, fname=f"../tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_cc.png")
-        draw_poses(img, persons_pred_gt, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt_labels.png",
-                   output_size=256)
+            if (pred==1).sum() == 0:
+                imgs_without_det += 1
+                continue
+            draw_clusters(img, joint_det, sparse_sol_gt, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt_full.png",
+                          output_size=256)
+            # draw_poses(imgs, persons_pred_cc, fname=f"../tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_cc.png")
+            draw_poses(img, persons_pred_gt, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt_labels.png",
+                       output_size=256)
 
-        # """
-    print(f"num images without detection :{imgs_without_det}")
+            # """
+        print(f"num images without detection :{imgs_without_det}")
 
 
 if __name__ == "__main__":