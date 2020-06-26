config for hidden layer of VanillaMPN masking crowds for keypoints_hourglass class fixed norm_factor (due to data types the distance edge features were zero) added end_with_relu option adapted eval model to hg models

src/Models/MessagePassingNetwork/VanillaMPN.py
@@ -52,7 +52,7 @@ default_config = {"steps": 10,
 class VanillaMPLayer(MessagePassing):
 
     # todo with or without inital feature skip connection
-    def __init__(self, node_feature_dim, edge_feature_dim, aggr, skip=False):
+    def __init__(self, node_feature_dim, edge_feature_dim, edge_feature_hidden, aggr, skip=False):
         super(VanillaMPLayer, self).__init__(aggr=aggr)
         # todo better architecture
 
@@ -60,9 +60,9 @@ class VanillaMPLayer(MessagePassing):
         node_factor = 2 if skip else 1
         edge_factor = 2 if skip else 1
 
-        self.mlp_edge = nn.Sequential(nn.Linear(node_feature_dim * 2 * node_factor + edge_feature_dim * edge_factor, 64),
+        self.mlp_edge = nn.Sequential(nn.Linear(node_feature_dim * 2 * node_factor + edge_feature_dim * edge_factor, edge_feature_hidden),
                                       non_lin,
-                                      nn.Linear(64, edge_feature_dim),
+                                      nn.Linear(edge_feature_hidden, edge_feature_dim),
                                       non_lin,
                                       )
 
@@ -96,12 +96,14 @@ class VanillaMPN(torch.nn.Module):
         super().__init__()
         # self.mpn = nn.ModuleList([VanillaMPLayer(node_feature_dim, edge_feature_dim) for i in range(steps)])
         self.use_skip_connections = config.SKIP
-        self.mpn = VanillaMPLayer(config.NODE_FEATURE_DIM, config.EDGE_FEATURE_DIM, aggr=config.AGGR, skip=config.SKIP)
+        self.mpn = VanillaMPLayer(config.NODE_FEATURE_DIM, config.EDGE_FEATURE_DIM, config.EDGE_FEATURE_HIDDEN, aggr=config.AGGR, skip=config.SKIP)
 
         # """
-        self.edge_embedding = _make_mlp(config.EDGE_INPUT_DIM, config.EDGE_EMB.OUTPUT_SIZES, False)
-        self.node_embedding = _make_mlp(config.NODE_INPUT_DIM, config.NODE_EMB.OUTPUT_SIZES, False)
-        self.classification = _make_mlp(config.EDGE_FEATURE_DIM, config.CLASS.OUTPUT_SIZES, False)
+        self.edge_embedding = _make_mlp(config.EDGE_INPUT_DIM, config.EDGE_EMB.OUTPUT_SIZES, bn=config.BN,
+                                        end_with_relu=config.NODE_EMB.END_WITH_RELU)
+        self.node_embedding = _make_mlp(config.NODE_INPUT_DIM, config.NODE_EMB.OUTPUT_SIZES, bn=config.BN,
+                                        end_with_relu=config.NODE_EMB.END_WITH_RELU)
+        self.classification = _make_mlp(config.EDGE_FEATURE_DIM, config.CLASS.OUTPUT_SIZES, bn=config.BN)
         # """
         non_linearity = nn.ReLU()
         """

src/Models/MessagePassingNetwork/VanillaMPN2.py
@@ -59,9 +59,12 @@ class VanillaMPN2(torch.nn.Module):
         self.mpn = nn.ModuleList([VanillaMPLayer2(config.NODE_FEATURE_DIM, config.NODE_FEATURE_DIM,
                                                   config.EDGE_FEATURE_DIM, config.EDGE_FEATURE_DIM, aggr=config.AGGR)
                                   for _ in range(config.STEPS)])
-        self.edge_embedding = _make_mlp(config.EDGE_INPUT_DIM, config.EDGE_EMB.OUTPUT_SIZES, bn=config.BN)
-        self.node_embedding = _make_mlp(config.NODE_INPUT_DIM, config.NODE_EMB.OUTPUT_SIZES, bn=config.BN)
-        self.classification = _make_mlp(config.EDGE_FEATURE_DIM, config.CLASS.OUTPUT_SIZES, bn=config.BN, init_trick=True)
+        self.edge_embedding = _make_mlp(config.EDGE_INPUT_DIM, config.EDGE_EMB.OUTPUT_SIZES, bn=config.BN,
+                                        end_with_relu=config.EDGE_EMB.END_WITH_RELU)
+        self.node_embedding = _make_mlp(config.NODE_INPUT_DIM, config.NODE_EMB.OUTPUT_SIZES, bn=config.BN,
+                                        end_with_relu=config.NODE_EMB.END_WITH_RELU)
+        self.classification = _make_mlp(config.EDGE_FEATURE_DIM, config.CLASS.OUTPUT_SIZES, bn=config.BN, init_trick=True,
+                                        end_with_relu=False)
 
         """
         self.mpn = nn.ModuleList([VanillaMPLayer2(config.NODE_FEATURE_DIM, config.NODE_FEATURE_DIM,

src/Models/MessagePassingNetwork/layers.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-def _make_mlp(input_dim, hidden_dims, bn=False, init_trick=False):
+def _make_mlp(input_dim, hidden_dims, bn=False, init_trick=False, end_with_relu=False):
     layers = []
     layers.append(nn.Linear(input_dim, hidden_dims[0]))
     if len(hidden_dims) != 1:
@@ -16,5 +16,9 @@ def _make_mlp(input_dim, hidden_dims, bn=False, init_trick=False):
             layers.append(nn.ReLU())
             if bn:
                 layers.append(nn.BatchNorm1d(hidden_dims[i]))
+    if end_with_relu:
+        layers.append(nn.ReLU())
+        if bn:
+            layers.append(nn.BatchNorm1d(hidden_dims[i]))
 
     return nn.Sequential(*layers)

src/data/CocoKeypoints_hg.py
@@ -14,7 +14,7 @@ from Utils.transformations import kpt_affine, factor_affine, get_transform
 class CocoKeypoints(Dataset):
 
     def __init__(self, path, mini=False, input_size=512, output_size=128, mode="train", seed=0, filter_empty=True,
-                 img_ids=None, year=14, transforms=None):
+                 img_ids=None, year=14, transforms=None, mask_crowds=True):
         np.random.seed(seed)
         torch.manual_seed(seed)
 
@@ -28,6 +28,7 @@ class CocoKeypoints(Dataset):
         # self.output_size = output_size
 
         self.max_num_people = 30  # from github code
+        self.mask_crowds = mask_crowds
         assert mode in ["train", "val"]
         self.data_dir =f"train20{year}" if mode == "train" else f"val20{year}"
 
@@ -101,16 +102,17 @@ class CocoKeypoints(Dataset):
 
         # load mask
         mask = np.zeros([img_height, img_width])
-        for j in ann:
-            if j["iscrowd"]:
-                assert j["num_keypoints"] == 0
-            # testing for the number of keypoints should be sufficient if the assertion above is true
-            if j["num_keypoints"] == 0:
-                encoding = maskapi.frPyObjects(j["segmentation"], img_height, img_width)
-                inst_mask = maskapi.decode(encoding)
-                if len(inst_mask.shape) == 3:
-                    inst_mask = maskapi.decode(encoding).sum(axis=2)
-                mask += inst_mask
+        if self.mask_crowds:
+            for j in ann:
+                if j["iscrowd"]:
+                    assert j["num_keypoints"] == 0
+                # testing for the number of keypoints should be sufficient if the assertion above is true
+                if j["num_keypoints"] == 0:
+                    encoding = maskapi.frPyObjects(j["segmentation"], img_height, img_width)
+                    inst_mask = maskapi.decode(encoding)
+                    if len(inst_mask.shape) == 3:
+                        inst_mask = maskapi.decode(encoding).sum(axis=2)
+                    mask += inst_mask
         mask = (mask < 0.5).astype(np.float32)
 
         """

src/graph_constructor/ConstructGraph.py
@@ -174,13 +174,15 @@ class NaiveGraphConstructor:
         if self.normalize_node_distance:
             norm_factor_x = self.scoremaps.shape[3]
             norm_factor_y = self.scoremaps.shape[2]
+            norm_factor = max(self.scoremaps.shape[3], self.scoremaps.shape[2])
         else:
             norm_factor_y, norm_factor_x = 1, 1
+            norm_factor = 1
 
-        edge_attr_y = (joint_y[edge_index[1]] - joint_y[edge_index[0]]) / norm_factor_y
-        edge_attr_x = (joint_x[edge_index[1]] - joint_x[edge_index[0]]) / norm_factor_x
+        edge_attr_y = (joint_y[edge_index[1]] - joint_y[edge_index[0]]).float() / norm_factor
+        edge_attr_x = (joint_x[edge_index[1]] - joint_x[edge_index[0]]).float() / norm_factor
 
-        edge_attr = torch.cat([edge_attr_x.unsqueeze(1), edge_attr_y.unsqueeze(1), connection_label_2], dim=1).float()
+        edge_attr = torch.cat([edge_attr_x.unsqueeze(1), edge_attr_y.unsqueeze(1), connection_label_2.float()], dim=1)
 
         return x, edge_attr, edge_index
 