configs BCE for edges Masking edge loss and correct loss calculation eval to log file saviing model after each scheduler step

src/Models/MessagePassingNetwork/ClassificationMPNSimple.py
@@ -86,17 +86,6 @@ class ClassificationMPNSimple(torch.nn.Module):
             node_features, edge_features = self.mpn_node_cls(node_features, edge_features, edge_index)
         preds_node.append(self.node_classification(node_features).squeeze())
 
-        true_positive_idx = preds_node[-1].sigmoid() > self.node_threshold
-        if kwargs["node_labels"] is not None and self.training:
-            true_positive_idx[kwargs["node_labels"] == 1.0] = True
-            true_positive_idx[kwargs["node_mask"] == 0.0] = False  # do not use ambiguous cases for edge classificatoni
-
-        mask = subgraph_mask(true_positive_idx, edge_index)
-        edge_features = edge_features[mask]
-
-        if len(edge_features) != 0:
-            preds_edge.append(self.edge_classification(edge_features).squeeze())
-        else:
-            preds_edge = [None]
+        preds_edge.append(self.edge_classification(edge_features).squeeze())
 
         return preds_edge, preds_node

src/Models/PoseEstimation/UpperBound.py
@@ -77,5 +77,5 @@ class UpperBoundModel(nn.Module):
 
         x, edge_attr, edge_index, edge_labels, node_labels, joint_det, label_mask, label_mask_node, joint_scores, _ = graph_constructor.construct_graph()
 
-        return scoremaps, edge_labels, node_labels, joint_det, joint_scores, edge_index, edge_labels, node_labels, label_mask, label_mask_node
+        return scoremaps, edge_labels, node_labels, joint_det, joint_scores, edge_index, edge_labels, node_labels, label_mask, label_mask_node, bb_output
 

src/Utils/Utils.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 from matplotlib import pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
-from torch_geometric.utils import dense_to_sparse, precision, recall, accuracy, f1_score
+from torch_geometric.utils import dense_to_sparse, precision, recall, accuracy, f1_score, subgraph
 
 from Utils.correlation_clustering.correlation_clustering_utils import cluster_graph
 from Utils.dataset_utils import Graph
@@ -123,6 +123,55 @@ def draw_detection_with_conf(img, joint_det, joint_score, joint_gt, fname=None, 
     else:
         raise NotImplementedError
 
+def draw_detection_scoremap(scoremaps, joint_det, joint_gt, inp_type, fname=None, output_size=128.0):
+    """
+    :param img: torcg.tensor. image
+    :param joint_det: shape: (num_joints, 2) list of xy positions of detected joints (without classes or clustering)
+    :param fname: optional - file name of image to save. If None the image is show with plt.show
+    :return:
+    """
+
+    scoremap = scoremaps.cpu().numpy().squeeze()
+    scoremap = scoremap[None, inp_type].clip(0, 1.0) * 255
+    scoremap = np.repeat(scoremap, repeats=3, axis=0).astype(np.uint8).transpose(1, 2, 0)
+    scoremap = cv2.resize(scoremap, (512, 512))
+
+    img = to_numpy(scoremap)
+    if img.dtype != np.uint8:
+        img = img * 255.0
+        img = img.astype(np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    colors_joints = np.arange(0, 179, np.ceil(179 / 17), dtype=np.float)
+    colors_joints[1::2] = colors_joints[-2::-2] # swap some colors to have clearer distinction between similar joint types
+
+    for i in range(len(joint_det)):
+        # scale up to 512 x 512
+        scale = 512.0 / output_size
+        x, y = int(joint_det[i, 0] * scale), int(joint_det[i, 1] * scale)
+        j_type = joint_det[i, 2]
+        if j_type == inp_type:
+            color = (colors_joints[j_type], 255, 255)
+            cv2.circle(img, (x, y), 2, color, -1)
+    for person in range(len(joint_gt)):
+        if np.sum(joint_gt[person]) > 0.0:
+            for i in range(len(joint_gt[person])):
+                # scale up to 512 x 512
+                scale = 512.0 / output_size
+                x, y = int(joint_gt[person, i, 0] * scale), int(joint_gt[person, i, 1] * scale)
+                j_type = i
+                if j_type == inp_type:
+                    cv2.circle(img, (x, y), 2, (120, 255, 255), -1)
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+
+    fig = plt.figure()
+    plt.imshow(img)
+    if fname is not None:
+        plt.savefig(fig=fig, fname=fname)
+        plt.close(fig)
+    else:
+        raise NotImplementedError
+
+
 def draw_detection(img, joint_det, joint_gt, fname=None, output_size=128.0):
     """
     :param img: torcg.tensor. image
@@ -218,6 +267,63 @@ def draw_poses(img: [torch.tensor, np.array], persons, fname=None, output_size=1
                 cv2.line(img, (x_1, y_1), (center_joint[0], center_joint[1]), color)
 
     img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+    fig = plt.figure()
+    plt.imshow(img)
+    if fname is not None:
+        plt.savefig(fig=fig, fname=fname)
+        plt.close(fig)
+    else:
+        raise NotImplementedError
+
+
+def draw_edges_conf(img, joint_det, person_labels, node_labels, edge_index, preds_edges, fname=None, output_size=128.0):
+    # filter nodes using the node labels -> subgraph
+    # for node in nodes
+    # draw node in color of cluster
+    # get all connections and draw them in color of confidence
+    tp_idx = node_labels > 0.5
+    mask = subgraph_mask(node_labels > 0.5, edge_index, )
+    edge_index, _ = subgraph(torch.from_numpy(tp_idx), torch.from_numpy(edge_index), None, relabel_nodes=True)
+    edge_index = edge_index.numpy()
+    joint_det = joint_det[tp_idx]
+    person_labels = person_labels[tp_idx]
+    preds_edges = preds_edges[mask]
+
+    img = to_numpy(img)
+    if img.dtype != np.uint8:
+        img = img * 255.0
+        img = img.astype(np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    colors_joints = np.linspace(0, 179, person_labels.max() + 1, dtype=np.float)
+    colors_edges = [0, 20, 50] #np.linspace(0, 179, 3, dtype=np.float)
+
+    for i in range(len(joint_det)):
+        scale = 512.0 / output_size
+        x, y = int(joint_det[i, 0] * scale), int(joint_det[i, 1] * scale)
+        cluster = person_labels[i]
+
+        color = (colors_joints[cluster], 255, 255)
+        cv2.circle(img, (x, y), 2, color, -1)
+        # draw connections
+        edges = edge_index[0] == i
+        sub_preds_edges = preds_edges[edges]
+        target_node_idxs = edge_index[1, edges]
+        for j in range(len(target_node_idxs)):
+            x_t, y_t = int(joint_det[j, 0] * scale), int(joint_det[j, 1] * scale)
+            conf = sub_preds_edges[j]
+            if conf < 0.33:
+                conf_class = 0
+            elif 0.33 <= conf <= 0.66:
+                conf_class = 2
+            elif conf > 0.66:
+                conf_class = 1
+            else:
+                raise NotImplementedError
+            color = (colors_edges[conf_class], 255, 255)
+            cv2.line(img, (x, y), (x_t, y_t), color)
+
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+
     fig = plt.figure()
     plt.imshow(img)
     if fname is not None:

src/Utils/loss.py
@@ -166,7 +166,7 @@ class ClassMPNLossFactory(nn.Module):
             self.edge_loss = FocalLoss(config.MODEL.LOSS.FOCAL_ALPHA, config.MODEL.LOSS.FOCAL_GAMMA,
                                        logits=True)
         else:
-            raise NotImplementedError
+            self.edge_loss = BCELossWtihLogits(pos_weight=1.0)
 
         if config.MODEL.LOSS.NODE_USE_FOCAL:
             self.node_loss = FocalLoss(config.MODEL.LOSS.FOCAL_ALPHA, config.MODEL.LOSS.FOCAL_GAMMA,
@@ -202,6 +202,7 @@ class FocalLoss(nn.Module):
         self.logits = logits
 
     def forward(self, inputs, targets, reduction, mask=None, batch_index=None):
+        assert batch_index is None
         if self.logits:
             BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
         else:
@@ -212,12 +213,10 @@ class FocalLoss(nn.Module):
             F_loss = F_loss * mask
 
         if reduction is not None:
-            if batch_index is not None and reduction == "mean":
-                F_loss = scatter_mean(F_loss, batch_index)
-                assert len(F_loss) == batch_index.max() + 1
+            if reduction == "mean" and mask is None:
                 return torch.mean(F_loss)
-            elif reduction == "mean" and batch_index is None:
-                return torch.mean(F_loss)
+            elif reduction == "mean" and mask is not None:
+                return torch.sum(F_loss) / mask.sum()
             elif reduction == "sum":
                 return torch.sum(F_loss)
         else:
@@ -230,9 +229,9 @@ class BCELossWtihLogits(nn.Module):
         self.loss = nn.BCEWithLogitsLoss(reduction="none")
 
     def forward(self, inputs, targets, reduction, mask=None, batch_index=None):
-        if mask is not None:
-            raise NotImplementedError
         bce_loss = self.loss(inputs, targets)
+        if mask is not None:
+            bce_loss = bce_loss * mask
         if self.pos_weight is not None:
             bce_loss[targets==1.0] *= self.pos_weight
         return bce_loss.mean()

src/eval_model_node.py
@@ -13,6 +13,45 @@ from Utils.transformations import reverse_affine_map
 from Utils.transforms import transforms_hg_eval, transforms_hr_eval
 
 
+class EvalWriter(object):
+
+    def __init__(self, config):
+        th = int(config.MODEL.MPN.NODE_THRESHOLD * 100)
+        self.f = open(config.LOG_DIR + f"/eval_{th:g}.txt", "w")
+    def eval_coco(self, coco, anns, ids, description):
+        print(description)
+        stats = coco_eval(coco, anns, ids)
+        self.f.write(description + "\n")
+        self.f.write(f"AP       : {stats[0]: 3f} \n")
+        self.f.write(f"AP    0.5: {stats[1]: 3f} \n")
+        self.f.write(f"AP   0.75: {stats[2]: 3f} \n")
+        self.f.write(f"AP medium: {stats[3]: 3f} \n")
+        self.f.write(f"AP  large: {stats[4]: 3f} \n")
+
+    def eval_metrics(self, eval_dict, descirption):
+        for k in eval_dict.keys():
+            eval_dict[k] = np.mean(eval_dict[k])
+        print(descirption)
+        print(eval_dict)
+        self.f.write(descirption + "\n")
+        self.f.write(str(eval_dict) + "\n")
+    def eval_part_metrics(self, eval_dict, description):
+        part_labels = ['nose','eye_l','eye_r','ear_l','ear_r',
+                       'sho_l','sho_r','elb_l','elb_r','wri_l','wri_r',
+                       'hip_l','hip_r','kne_l','kne_r','ank_l','ank_r']
+        for i in range(17):
+            for k in eval_dict[i].keys():
+                eval_dict[i][k] = np.mean(eval_dict[i][k])
+        print(description)
+        self.f.write(description + " \n")
+        for i in range(17):
+            string = f"{part_labels[i]} acc: {eval_dict[i]['acc']:3f} prec: {eval_dict[i]['prec']:3f} rec: {eval_dict[i]['rec']:3f} f1: {eval_dict[i]['f1']:3f}"
+            print(string)
+            self.f.write(string + "\n")
+    def close(self):
+        self.f.close()
+
+
 def specificity(pred, target, num_classes):
     r"""Computes the recall
     :math:`\frac{\mathrm{TP}}{\mathrm{TP}+\mathrm{FN}}` of predictions.
@@ -110,9 +149,10 @@ def main():
     torch.backends.cudnn.benchmark = False
     ######################################
 
-    config_name = "model_41_0"
+    config_name = "model_50_2"
     config = get_config()
     config = update_config(config, f"../experiments/train/{config_name}.yaml")
+    eval_writer = EvalWriter(config)
 
     if config.TEST.SPLIT == "mini":
         train_ids, valid_ids = pickle.load(open("tmp/mini_train_valid_split_4.p", "rb"))
@@ -177,14 +217,12 @@ def main():
             preds_edges = pred[-1].sigmoid().squeeze() if pred[-1] is not None else None
             preds_nodes = preds_nodes[-1].sigmoid().squeeze()
 
-            true_positive_idx = preds_nodes > 0.5
+            true_positive_idx = preds_nodes > config.MODEL.MPN.NODE_THRESHOLD
             mask = subgraph_mask(true_positive_idx, edge_index)
-            result_edges = torch.zeros(edge_index.shape[1], dtype=torch.float, device=edge_index.device)
-            if preds_edges is not None:
-                result_edges[mask] = preds_edges
-            result_edges = torch.where(result_edges < 0.5, torch.zeros_like(result_edges), torch.ones_like(result_edges))
+            result_edges = preds_edges * mask.float()
 
-            result_nodes = torch.where(preds_nodes < 0.5, torch.zeros_like(preds_nodes), torch.ones_like(preds_nodes))
+            result_edges = torch.where(result_edges < 0.5, torch.zeros_like(result_edges), torch.ones_like(result_edges))
+            result_nodes = torch.where(preds_nodes < config.MODEL.MPN.NODE_THRESHOLD, torch.zeros_like(preds_nodes), torch.ones_like(preds_nodes))
             n, _ = num_non_detected_points(joint_det.cpu(), keypoints.cpu(), 6.0, config.MODEL.GC.USE_GT)
 
             eval_node = merge_dicts(eval_node, calc_metrics(result_nodes, node_labels))
@@ -198,87 +236,36 @@ def main():
             img_info = eval_set.coco.loadImgs(int(eval_set.img_ids[i]))[0]
 
             ann = perd_to_ann(scoremaps[0], joint_det, preds_nodes, edge_index, preds_edges, img_info, int(eval_set.img_ids[i]), config.MODEL.GC.CC_METHOD
-                              , config.DATASET.SCALING_TYPE, config.TEST.ADJUST)
-            perf_edges = edge_labels[mask]
-            ann_perf_edge = perd_to_ann(scoremaps[0], joint_det, preds_nodes, edge_index, perf_edges, img_info, int(eval_set.img_ids[i]), config.MODEL.GC.CC_METHOD
-                              , config.DATASET.SCALING_TYPE, config.TEST.ADJUST)
+                              , config.DATASET.SCALING_TYPE, config.TEST.ADJUST, config.MODEL.MPN.NODE_THRESHOLD)
+
+            ann_perf_edge = perd_to_ann(scoremaps[0], joint_det, preds_nodes, edge_index, edge_labels, img_info, int(eval_set.img_ids[i]), config.MODEL.GC.CC_METHOD
+                              , config.DATASET.SCALING_TYPE, config.TEST.ADJUST, config.MODEL.MPN.NODE_THRESHOLD)
+            ann_perf_node = perd_to_ann(scoremaps[0], joint_det, node_labels, edge_index, preds_edges, img_info, int(eval_set.img_ids[i]), config.MODEL.GC.CC_METHOD
+                                        , config.DATASET.SCALING_TYPE, config.TEST.ADJUST, config.MODEL.MPN.NODE_THRESHOLD)
 
             anns.append(ann)
             anns_perf_edge.append(ann_perf_edge)
+            anns_perf_node.append(ann_perf_node)
             if int(n) == 0:
                 imgs_fully_det.append(eval_set.img_ids[i])
                 anns_full.append(ann)
-        model.train()
-        model.stop_backbone_bn()
-        for i in tqdm(range(config.TEST.NUM_EVAL)):
-            eval_ids.append(eval_set.img_ids[i])
-
-            img, _, masks, keypoints, factors = eval_set[i]
-            img = img.to(device)[None]
-            masks, keypoints, factors = to_tensor(device, masks[-1], keypoints, factors)
-
-            scoremaps, pred, preds_nodes, joint_det, joint_scores, edge_index, edge_labels, node_labels, _, node_mask, _ = model(
-                img, keypoints, masks, factors, with_logits=True)
-
-            preds_edges = pred[-1].sigmoid().squeeze() if pred[-1] is not None else None
-            preds_nodes = preds_nodes[-1].sigmoid().squeeze()
-
-            true_positive_idx = preds_nodes > 0.5
-            true_positive_idx[node_labels == 1.0] = True
-            true_positive_idx[node_mask == 0.0] = False
-            mask = subgraph_mask(true_positive_idx, edge_index)
-            result_edges = torch.zeros(edge_index.shape[1], dtype=torch.float, device=edge_index.device)
-            if pred[-1] is not None:
-                result_edges[mask] = preds_edges
-
-            _, preds_edges = subgraph(node_labels > 0.5, edge_index, result_edges)
-
-            img_info = eval_set.coco.loadImgs(int(eval_set.img_ids[i]))[0]
-
-            ann_perf_node = perd_to_ann(scoremaps[0], joint_det, node_labels, edge_index, preds_edges, img_info,
-                              int(eval_set.img_ids[i]), config.MODEL.GC.CC_METHOD
-                              , config.DATASET.SCALING_TYPE, config.TEST.ADJUST)
-
-            anns_perf_node.append(ann_perf_node)
 
         print("##################")
-        print("General Evaluation")
-        coco_eval(eval_set.coco, anns, np.array(eval_ids))
-        print("Perfect edge prediction")
-        coco_eval(eval_set.coco, anns_perf_edge, np.array(eval_ids))
-        print("Perfect node prediction")
-        coco_eval(eval_set.coco, anns_perf_node, np.array(eval_ids))
-        print("##################")
-        print("Real Evaluation on perfect images")
-        print(f"Number of perfect images: {len(anns_full)}")
-        coco_eval(eval_set.coco, anns_full, imgs_fully_det)
-        for k in eval_edge.keys():
-            eval_edge[k] = np.mean(eval_edge[k])
-        for k in eval_node.keys():
-            eval_node[k] = np.mean(eval_node[k])
-        for i in range(17):
-            for k in eval_node_per_type[i].keys():
-                eval_node_per_type[i][k] = np.mean(eval_node_per_type[i][k])
-        print("node metrics")
-        print(eval_node)
-        print("edge metrics")
-        print(eval_edge)
-        print("node metrics per type")
-        part_labels = ['nose','eye_l','eye_r','ear_l','ear_r',
-               'sho_l','sho_r','elb_l','elb_r','wri_l','wri_r',
-               'hip_l','hip_r','kne_l','kne_r','ank_l','ank_r']
-        for i in range(17):
-            print(f"{part_labels[i]} "
-                  f"acc: {eval_node_per_type[i]['acc']:3f}  "
-                  f"prec: {eval_node_per_type[i]['prec']:3f} "
-                  f"rec: {eval_node_per_type[i]['rec']:3f} "
-                  f"f1: {eval_node_per_type[i]['f1']:3f}")
+        eval_writer.eval_coco(eval_set.coco, anns, np.array(eval_ids), "General Evaluation")
+        eval_writer.eval_coco(eval_set.coco, anns_perf_edge, np.array(eval_ids), "Perfect edge prediction")
+        eval_writer.eval_coco(eval_set.coco, anns_perf_node, np.array(eval_ids), "Perfect node prediction")
+        eval_writer.eval_coco(eval_set.coco, anns_full, imgs_fully_det, f"Evaluation on perfect images {len(anns_full)}")
+
+        eval_writer.eval_metrics(eval_node, "Node metrics")
+        eval_writer.eval_metrics(eval_edge, "Edge metrics")
+        eval_writer.eval_part_metrics(eval_node_per_type, "Node metrics per type")
+
+        eval_writer.close()
 
 
-
-def perd_to_ann(scoremaps, joint_det, joint_scores, edge_index, pred, img_info, img_id, cc_method, scaling_type, adjustment):
-    true_positive_idx = joint_scores > 0.5
-    edge_index, _ = subgraph(true_positive_idx, edge_index)
+def perd_to_ann(scoremaps, joint_det, joint_scores, edge_index, pred, img_info, img_id, cc_method, scaling_type, adjustment, th):
+    true_positive_idx = joint_scores > th
+    edge_index, pred = subgraph(true_positive_idx, edge_index, pred)
     if edge_index.shape[1] != 0:
         persons_pred, _, _ = pred_to_person(joint_det, joint_scores, edge_index, pred, cc_method)
     else:

src/test/test_construct_graph.py
@@ -5,7 +5,7 @@ import pickle
 from config import get_config, update_config
 from data import CocoKeypoints_hg, CocoKeypoints_hr, HeatmapGenerator
 from Utils.transforms import transforms_hr_train, transforms_hr_eval, transforms_hg_eval
-from Utils import draw_detection, draw_clusters, draw_poses, pred_to_person, graph_cluster_to_persons, to_device, to_tensor
+from Utils import draw_detection, draw_clusters, draw_poses, pred_to_person, graph_cluster_to_persons, to_device, to_tensor, draw_detection_scoremap
 from Models import get_upper_bound_model
 import matplotlib;matplotlib.use("Agg")
 
@@ -40,7 +40,7 @@ def main():
 
     imgs_without_det = 0
     with torch.no_grad():
-        for i in range(3500):  # just test the first 100 images
+        for i in range(100):  # just test the first 100 images
             # split batch
             img_id = img_set.img_ids[i]
             if use_subset:
@@ -48,10 +48,10 @@ def main():
                     continue
             print(f"Iter : {i}")
             print(f"img_idx: {img_set.img_ids[i]}")
-            img, _, masks, keypoints, factor_list = img_set[i]
+            img, heatmaps, masks, keypoints, factor_list = img_set[i]
             mask, keypoints, factor_list = to_tensor(device, masks[-1], keypoints, factor_list)
             img = img.to(device)[None]
-            _, pred, preds_nodes, joint_det, joint_scores, edge_index, _, _, label_mask, _ = model(img, keypoints, mask, factor_list)
+            sm_avg, pred, preds_nodes, joint_det, joint_scores, edge_index, _, _, label_mask, _, scoremaps = model(img, keypoints, mask, factor_list)
 
             # construct poses
             persons_pred_cc, _, _ = pred_to_person(joint_det, joint_scores, edge_index, pred, config.MODEL.GC.CC_METHOD)
@@ -67,10 +67,26 @@ def main():
             clean_joint_det = joint_det[preds_nodes == 1]
             keypoints = keypoints.cpu().numpy().squeeze()
 
+            heatmaps = torch.from_numpy(heatmaps[1])
             img = np.array(transforms_inv(img.cpu().squeeze()))
+            """
             draw_detection(img, joint_det, keypoints,
                            fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_det.png",
                            output_size=256)
+            """
+            draw_detection_scoremap(heatmaps, joint_det[preds_nodes==1.0], keypoints, 6,
+                           fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_shoulder.png",
+                           output_size=256)
+            draw_detection_scoremap(sm_avg[0], joint_det[preds_nodes==1.0], keypoints, 6,
+                                    fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_shoulder_avg.png",
+                                    output_size=256)
+            draw_detection_scoremap(sm_avg[0], joint_det[preds_nodes==1.0], keypoints, 11,
+                                    fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_hip_avg.png",
+                                    output_size=256)
+            draw_detection_scoremap(heatmaps, joint_det[preds_nodes==1.0], keypoints, 11,
+                                    fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_hip.png",
+                                    output_size=256)
+            """
             draw_detection(img, clean_joint_det, keypoints,
                            fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_clean.png",
                            output_size=256)
@@ -84,6 +100,7 @@ def main():
             # draw_poses(imgs, persons_pred_cc, fname=f"../tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_cc.png")
             draw_poses(img, persons_pred_gt, fname=f"tmp/test_construct_graph_img/{img_set.img_ids[i]}_pose_gt_labels.png",
                        output_size=256)
+            """
 
             # """
         print(f"num images without detection :{imgs_without_det}")

src/test/test_train_loop.py
@@ -60,14 +60,14 @@ def make_train_func(model, optimizer, loss_func, **kwargs):
                 true_positive_idx = preds_nodes[i].sigmoid() > kwargs["config"].MODEL.MPN.NODE_THRESHOLD
                 true_positive_idx[node_labels == 1.0] = True
                 mask = subgraph_mask(true_positive_idx, edge_index)
-                loss_edge_labels.append(edge_labels[mask])
-                loss_masks.append(label_mask[mask])
+                loss_edge_labels.append(edge_labels)
+                loss_masks.append(label_mask * mask.float())
 
             loss, _ = loss_func(preds, preds_nodes, loss_edge_labels, node_labels, loss_masks, label_mask_node)
 
             #
             default_pred = torch.zeros(edge_index.shape[1], dtype=torch.float, device=edge_index.device) - 1.0
-            default_pred[mask] = preds[-1].detach()
+            default_pred[mask] = preds[-1][mask].detach()
 
         else:
             raise NotImplementedError

src/train.py
@@ -79,24 +79,18 @@ def make_train_func(model, optimizer, loss_func, **kwargs):
             elif isinstance(loss_func, ClassMultiLossFactory):
                 loss_masks = []
                 loss_edge_labels = []
+                # first the graph reduction
+                mask = None
                 for i in range(len(preds_nodes)):
                     true_positive_idx = preds_nodes[i].sigmoid() > kwargs["config"].MODEL.MPN.NODE_THRESHOLD
                     true_positive_idx[node_labels == 1.0] = True
-                    true_positive_idx[label_mask_node == 0.0] = False
                     mask = subgraph_mask(true_positive_idx, edge_index)
-                    loss_edge_labels.append(edge_labels[mask])
-                    loss_masks.append(label_mask[mask])
+                    loss_edge_labels.append(edge_labels)
+                    loss_masks.append(label_mask * mask.float())
 
                 loss, _ = loss_func(bb_output, preds_nodes, preds, heatmaps, node_labels, loss_edge_labels, masks,
                                  loss_masks, label_mask_node)
-                #
-                default_pred = torch.zeros(edge_index.shape[1], dtype=torch.float,
-                                           device=edge_index.device) - 1.0
-                if preds is not None:
-                    default_pred[mask] = preds[-1].detach()
-                    preds[-1] = default_pred
-                else:
-                    preds = [default_pred]
+                label_mask = label_mask * mask.float()
             else:
                 raise NotImplementedError
 
@@ -124,35 +118,18 @@ def make_train_func(model, optimizer, loss_func, **kwargs):
                 for i in range(len(preds_nodes)):
                     true_positive_idx = preds_nodes[i].sigmoid() > kwargs["config"].MODEL.MPN.NODE_THRESHOLD
                     true_positive_idx[node_labels == 1.0] = True
-                    true_positive_idx[label_mask_node== 0.0] = False
                     mask = subgraph_mask(true_positive_idx, edge_index)
-                    loss_edge_labels.append(edge_labels[mask])
-                    loss_masks.append(label_mask[mask])
+                    loss_edge_labels.append(edge_labels)
+                    loss_masks.append(label_mask * mask.float())
 
                 loss, _ = loss_func(preds, preds_nodes, loss_edge_labels, node_labels, loss_masks, label_mask_node)
-                #
-                default_pred = torch.zeros(edge_index.shape[1], dtype=torch.float,
-                                           device=edge_index.device) - 1.0
-                default_pred[mask] = preds[-1].detach()
-                # """
-                """
-                true_positive_idx = preds_nodes[-1].detach() > 0.0
-                true_positive_idx[node_labels == 1.0] = True
-                mask = subgraph_mask(true_positive_idx, edge_index)
-                loss_mask = label_mask.clone() # .[mask == 0] = 0.0
-                loss_mask[mask==0] = 0.0
-                loss = loss_func(preds, preds_nodes, edge_labels, node_labels, loss_mask)
-                label_mask[mask == 0] = 1.0
-                # """
+                label_mask = label_mask * mask.float()
             else:
                 raise NotImplementedError
             loss.backward()
             optimizer.step()
         loss = loss.item()
-        if isinstance(loss_func, ClassMPNLossFactory):
-            preds_edges = default_pred
-        else:
-            preds_edges = preds[-1].detach()
+        preds_edges = preds[-1].detach()
         preds_nodes = None if preds_nodes is None else preds_nodes[-1].detach()
         edge_labels = edge_labels.detach()
         node_labels = None if preds_nodes is None else node_labels.detach()
@@ -246,7 +223,7 @@ def main():
             result_edges = preds_edges.sigmoid().squeeze()
             result_edges = torch.where(result_edges < 0.5, torch.zeros_like(result_edges), torch.ones_like(result_edges))
 
-            node_metrics = calc_metrics(result_nodes, node_labels, node_label_mask)  #  not sure
+            node_metrics = calc_metrics(result_nodes, node_labels, node_label_mask)
             edge_metrics = calc_metrics(result_edges, edge_labels, edge_label_mask)
 
             logger.log_vars("Metric/train", iter, **edge_metrics)
@@ -272,7 +249,8 @@ def main():
         print("#### BEGIN VALIDATION ####")
 
         valid_dict = {"node": {"acc": [], "prec": [], "rec": [], "f1": []},
-                      "edge": {"acc": [], "prec": [], "rec": [], "f1": []}}
+                      "edge": {"acc": [], "prec": [], "rec": [], "f1": []},
+                      "edge_masked": {"prec": [], "rec": []}}
         valid_loss = []
         valid_heatmap = []
         with torch.no_grad():
@@ -291,21 +269,24 @@ def main():
                 if config.TRAIN.END_TO_END:
                     if isinstance(loss_func, MultiLossFactory):
                         loss, logging = loss_func(bb_output, preds, heatmaps, edge_labels, masks, label_mask)
+                        loss_mask = label_mask
                     elif isinstance(loss_func, ClassMultiLossFactory):
                         loss_masks = []
                         loss_edge_labels = []
                         for i in range(len(preds_nodes)):
-                            true_positive_idx = preds_nodes[i].sigmoid() > config.MODEL.MPN.NODE_THRESHOLD
+                            true_positive_idx = preds_nodes[i].sigmoid() > 0.5
                             mask = subgraph_mask(true_positive_idx, edge_index)
-                            loss_edge_labels.append(edge_labels[mask])
-                            loss_masks.append(label_mask[mask])
+
+                            loss_edge_labels.append(edge_labels)
+                            loss_masks.append(label_mask * mask.float())
 
                         loss, logging = loss_func(bb_output, preds_nodes, preds, heatmaps, node_labels, loss_edge_labels, masks, loss_masks, label_mask_node)
                         #
+                        loss_mask = loss_masks[-1].detach()
                         default_pred = torch.zeros(edge_index.shape[1], dtype=torch.float,
                                                    device=edge_index.device) - 1.0
                         if preds[-1] is not None:
-                            default_pred[mask] = preds[-1].detach()
+                            default_pred[mask] = preds[-1][mask].detach()
                             preds[-1] = default_pred
                         else:
                             preds = [default_pred]
@@ -313,36 +294,28 @@ def main():
                     if isinstance(loss_func, MPNLossFactory):
 
                         loss, logging = loss_func(preds, edge_labels, label_mask=label_mask)
+                        loss_mask = label_mask
                     elif isinstance(loss_func, ClassMPNLossFactory):
                         # adapt labels and mask to reduced graph
                         # """
                         loss_masks = []
                         loss_edge_labels = []
                         for i in range(len(preds_nodes)):
-                            true_positive_idx = preds_nodes[i].sigmoid() > config.MODEL.MPN.NODE_THRESHOLD
+                            true_positive_idx = preds_nodes[i].sigmoid() > 0.5
                             mask = subgraph_mask(true_positive_idx, edge_index)
-                            loss_edge_labels.append(edge_labels[mask])
-                            loss_masks.append(label_mask[mask])
+                            loss_edge_labels.append(edge_labels)
+                            loss_masks.append(label_mask * mask.float())
 
                         loss, logging = loss_func(preds, preds_nodes, loss_edge_labels, node_labels, loss_masks, label_mask_node)
                         #
                         default_pred = torch.zeros(edge_index.shape[1], dtype=torch.float,
                                                    device=edge_index.device) - 1.0
+                        loss_mask = loss_masks[-1].detach()
                         if preds[-1] is not None:
-                            default_pred[mask] = preds[-1].detach()
+                            default_pred[mask] = preds[-1][mask].detach()
                             preds[-1] = default_pred
                         else:
                             preds = [default_pred]
-                        # """
-                        """
-                        true_positive_idx = preds_nodes[-1].detach() > 0.0
-                        true_positive_idx[node_labels == 1.0] = True
-                        mask = subgraph_mask(true_positive_idx, edge_index)
-                        loss_mask = label_mask.clone()  # .[mask == 0] = 0.0
-                        loss_mask[mask == 0] = 0.0
-                        loss = loss_func(preds, preds_nodes, edge_labels, node_labels, loss_mask)
-                        label_mask[mask == 0] = 1.0
-                        # """
                     else:
                         raise NotImplementedError
 
@@ -356,20 +329,19 @@ def main():
                     result_nodes = None
                     node_labels = None
 
-                # remove masked connections from score calculation
-                """
-                if len(result) == 0:
-                    continue
-                """
                 node_metrics = calc_metrics(result_nodes, node_labels, label_mask_node)
-                edge_metrics = calc_metrics(result_edges, edge_labels, label_mask)
+                edge_metrics_complete = calc_metrics(result_edges, edge_labels, label_mask)
+                edge_metrics_masked = calc_metrics(result_edges, edge_labels, loss_mask)
 
                 valid_loss.append(loss.item())
                 if "heatmap" in logging:
                     valid_heatmap.append(logging["heatmap"])
-                if edge_metrics is not None:
-                    for key in edge_metrics.keys():
-                        valid_dict["edge"][key].append((edge_metrics[key]))
+                if edge_metrics_complete is not None:
+                    for key in edge_metrics_complete.keys():
+                        valid_dict["edge"][key].append((edge_metrics_complete[key]))
+                if edge_metrics_masked is not None:
+                    for key in ["prec", "rec"]:
+                        valid_dict["edge_masked"][key].append((edge_metrics_masked[key]))
 
                 if node_metrics is not None:
                     for key in node_metrics.keys():
@@ -384,6 +356,7 @@ def main():
         logger.log_loss(np.mean(valid_loss), "Loss/valid", epoch)
         logger.log_loss(np.mean(valid_heatmap), "Loss/valid_heat", epoch)
         logger.log_vars("Metric/valid", epoch, **valid_dict['edge'])
+        logger.log_vars("Metric/valid_masked", epoch, **valid_dict['edge_masked'])
         logger.log_vars("Metric/Node/valid", epoch, **valid_dict['node'])
 
         torch.save({"epoch": epoch,
@@ -391,6 +364,13 @@ def main():
                     "optimizer_state_dict": optimizer.state_dict(),
                     "lr_scheduler_state_dict": scheduler.state_dict()
                     }, config.MODEL.PRETRAINED)
+        if epoch + 1 in config.TRAIN.LR_STEP:
+            save_name = config.LOG_DIR + f"/pose_estimation_epoch_{epoch}.pth"
+            torch.save({"epoch": epoch,
+                        "model_state_dict": model.state_dict(),
+                        "optimizer_state_dict": optimizer.state_dict(),
+                        "lr_scheduler_state_dict": scheduler.state_dict()
+                        }, save_name)
 
 
 if __name__ == "__main__":